a272edffef7be4a18f758f993cfa397f31614314
CJohnston079 <chris79gp@outlook.com>
2023-04-07 15:49:27 +0100

    added functionality to paint on canvas while clicking and dragging

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let gridArea = 0;
 generateGrid(8)
 
 function generateGrid(gridSize) {
-    if (gridSize < 4 || gridSize > 64) return
+    if (gridSize < 4 || gridSize > 32) return
     resetGrid()
     let gridArea = gridSize*gridSize
     for (let i = gridArea; i > 0; i--) {
-        generatePixels()
+        generatePixels(i)
     }
     canvas.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
     return gridArea;
@@ -21,10 +21,42 @@ function resetGrid() {
       });
 }
 
-function generatePixels() {
-    let div = document.createElement('div');
-    div.classList.add('pixel');
-    canvas.appendChild(div);
-    pixels = document.querySelectorAll('.pixel');
-    return pixels
+function generatePixels(i) {
+    setTimeout(() => {
+        let div = document.createElement('div');
+        div.classList.add('pixel');
+        canvas.appendChild(div);
+        pixels = document.querySelectorAll('.pixel');
+        return pixels
+    }, 1 * i)
+}
+
+// paint functions
+
+canvas.addEventListener('mousedown', enablePainting)
+canvas.addEventListener('mouseup', disablePainting)
+
+let mousedown = false
+
+function enablePainting() {
+    mousedown = true
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mousemove', () => {
+            paintPixel(pixel)
+        })
+        pixel.addEventListener('mouseup', () => {
+            paintPixel(pixel)
+        })
+    });
+}
+
+function paintPixel(pixel) {
+    if (mousedown === true) {
+        pixel.style.backgroundColor = 'black';
+    }
+}
+
+function disablePainting() {
+    mousedown = false
+    return mousedown
 }
